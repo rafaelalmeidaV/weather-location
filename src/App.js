eb44a93641846b4e8aad7400470f0dc61971545c
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2023-09-23 10:15:03 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    })
+  }, []);
+
+  const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+
+  let getWeather = async (lat, long) => {
+    try {
+      let res = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+          lat: lat,
+          lon: long,
+          appid: apiKey,
+          lang: 'pt',
+          units: 'metric'
+        }
+      });
+      setWeather(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error('Erro ao buscar dados meteorológicos:', error);
+    }
+  }
+
+  if (!location) {
+    return (
+      <Fragment>
+        Você precisa habilitar a localização no navegador.
+      </Fragment>
+    );
+  } else if (!weather) {
+    return (
+      <Fragment>
+        Carregando dados meteorológicos...
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        <h3>Clima nas suas Coordenadas ({weather.weather[0].description})</h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual: {weather.main.temp}°</li>
+          <li>Temperatura máxima: {weather.main.temp_max}°</li>
+          <li>Temperatura mínima: {weather.main.temp_min}°</li>
+          <li>Pressão: {weather.main.pressure} hpa</li>
+          <li>Umidade: {weather.main.humidity}%</li>
+        </ul>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
